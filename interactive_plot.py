1b03fbe9f55a46da7b08052da8312b8d1289fcd0
Max Bramberger <Max.Bramberger@t-online.de>
2023-02-04 17:23:26 +0100

    Customer wants probability in percent

--- interactive_plot.py
+++ interactive_plot.py
@@ -12,14 +12,16 @@ def get_interactive_plot_fire_bolt_vs_eldritch_blast(color: str= "red"):
     eldritch_blast=EldritchBlast(attack_bonus=7,armor_class=17,base_damage=4,number_of_attacks=2)
     fire_bolt=FireBolt(attack_bonus=7,armor_class=17,base_damage=4,number_of_dice=2)
     scope, distribution=eldritch_blast.get_damage_distribution()
+    distribution*=100
     scope2, distribution2=fire_bolt.get_damage_distribution()
+    distribution2 *= 100
     fig=plt.figure()
     ax=fig.add_subplot(111)
     fig.subplots_adjust(left=0.25,bottom=0.25)
     line,=ax.plot(scope,distribution,marker="x", linestyle="none",color=color, label="Eldritch Blast")
     line2,=ax.plot(scope2,distribution2,marker="x",linestyle="none",color="blue", label="Fire Bolt")
     plt.title("Base dmg: 4, AC: 17, number of dice/attacks: 2")
-    plt.ylabel("Probability")
+    plt.ylabel(r"Probability [%]")
     plt.xlabel("Damage")
     plt.legend()
 
@@ -29,10 +31,12 @@ def get_interactive_plot_fire_bolt_vs_eldritch_blast(color: str= "red"):
     def sliders_on_changed(val):
         eldritch_blast = EldritchBlast(attack_bonus=int(atk_bonus_slider.val), armor_class=17, base_damage=4, number_of_attacks=2)
         scope, distribution = eldritch_blast.get_damage_distribution()
+        distribution *= 100
         line.set_data(scope,distribution)
         fire_bolt = FireBolt(attack_bonus=int(atk_bonus_slider.val), armor_class=17, base_damage=4,
                                        number_of_dice=2)
         scope, distribution = fire_bolt.get_damage_distribution()
+        distribution *= 100
         line2.set_data(scope, distribution)
         fig.canvas.draw_idle()
     atk_bonus_slider.on_changed(sliders_on_changed)
